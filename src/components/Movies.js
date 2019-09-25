e6f0caf5ae78e42ad43f192c93c70a4978eecb3d
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 21:41:34 +0000

    Automatically backed up by Learn

---
+++ src/components/Movies.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { movies } from '../data';
+
+const Movies = () => {
+  return (
+    <div>
+        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) =>
+        <div>{movie.title} {movie.time}
+          <ul>{movie.genres.map((genre) => (<li>{genre}</li>))}</ul>
+        </div>)}
+    </div>
+  );
+};
+
+export default Movies;
