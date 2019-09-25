6eda0479ce5a5ea559317a4d489ec329d8e34086
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 21:40:54 +0000

    Automatically backed up by Learn

---
+++ src/components/Directors.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { directors } from '../data';
+
+const Directors = () => {
+
+  
+
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) =>
+        <div>{director.name}
+          <ul>{director.movies.map((movie) => (<li>{movie}</li>))}</ul>
+        </div>)}
+    </div>
+  );
+}
+
+export default Directors
